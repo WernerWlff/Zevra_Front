73eb20dba839bcca1bf6b932f4774cfc94be821e
Werner Wolff <werner.wolff1109@gmail.com>
2026-01-26 15:30:05 +0100

    fix: during the PR of login and register, i had to delete the route for register

--- Zevra/src/app/app.routes.ts
+++ Zevra/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FrontPage } from './components/front-page/front-page';
 import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
 export const routes: Routes = [
     {path: '', component: FrontPage},
     {path: 'login', component: Login},
+    {path: 'register', component: Register},
 ];

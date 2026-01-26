87e42b5c327042e5f5f1e6742dd82c9e5d869c0c
Werner Wolff <werner.wolff1109@gmail.com>
2026-01-26 14:33:58 +0100

    feat: register page - added the route inside app.routes
    fix: quick reformating of the frontPage file

--- Zevra/src/app/app.routes.ts
+++ Zevra/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FrontPage } from './components/front-page/front-page';
+import { Register } from './components/register/register';
 
 export const routes: Routes = [
     {path: '', component: FrontPage},
+    {path: 'register', component: Register},
 ];
